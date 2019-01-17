2d55263e931cb7df5861943c3803ef972db6b612
tobyatgithub <toby.fangyuan@gmail.com>
2019-01-17 09:58:55 -0800

    1 17 2019 check point

--- data_structures/hash_table/hash_table.py
+++ data_structures/hash_table/hash_table.py
@@ -1,20 +1,57 @@
 class HashTable:
-    def __init__(self, size=11):
-        self.size = size  # number of buckets
-        self.table = [None] * size
-        # self.keys = [None] * size
+    """
+    A class for a hash table.
+    """
+
+    def __init__(self, size=8192):
+        self.entries_count = 0
+        self.alphabet_size = 52
+        self.table_size = size
+        self.hashtable = [[] for _ in range(size)]
         # usually we would like size to be a prime number
         #  to reduce chance of data collision
 
-    def put(self, data):
+    def __repr__(self):
+        return "<HashTable: {}>".format(self.hashtable)
+
+    def __len__(self):
+        count = 0
+        for item in self.hashtable:
+            count += len(item)
+        return count
+
+    def _hash_key(self, key):
         """
-        Given a hash table class, and a data point to
-        put into this hash table. This function will
-        generate the key for this data according to
-        self.size and store that data into it.
-        Data collision is solved by a list.
+        takes in string key and turn that into a hash result
         """
-        key = data % self.size
-        self.table.setdefault(key,[])
-        self.table[key].append(data)
-        return self
+        _key = ''.join(str(ord(c)) for c in key)
+        return int(_key) % self.table_size
+
+    def put(self, key, value):
+        """
+        Add a key and value into the hash table
+        """
+        if not isinstance(key, str):
+            raise TypeError("Only strings can be used as keys")
+        hash_ = self._hash_key(key)
+        for i, item in enumerate(self.hashtable[hash_]):
+            if item[0] == key:
+                del self.hashtable[hash_][i]
+                self.entries_count -= 1
+        self.hashtable[hash_].append((key, value))
+        self.entries_count += 1
+
+    def get(self, key):
+        """
+        Retrieve a value from the hash table by key.
+        """
+        hash_ = self._hash_key(key)
+        for i, item in enumerate(self.hashtable[hash_]):
+            # why enumerate as we don't really use i?
+            if item[0] == key:
+                return item[1]
+        # raise KeyError("Key not in hash table.")
+        return False
+
+
+
